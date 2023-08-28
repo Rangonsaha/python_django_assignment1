6b8329c4791dcce2cf229c3e513d97afabe8405b
Rangon Kumar Shaha <107715292+Rangonsaha@users.noreply.github.com>
2023-08-28 16:13:33 +0600

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,53 @@
+#1.print your name
+print('Rangon Kumar Saha')
+
+#2.input your batch number using input() function
+Batch = int(input('Enter Your Batch Number : '))
+print('Your Batch number is:',Batch)
+
+#3.create 5 different valid variable and print
+Name = "Rangon Kumar Saha"
+age = 21
+cgpa = 3.5
+university = "American International University Bangladesh"
+dept = "Computer Science And Engineering"
+
+print("My name is "+Name)
+print("I am ",age," years old")
+print("I studies in "+university+" department of "+dept)
+print("My cgpa is ",cgpa)
+
+#4.create pascal case variable
+MyHome = "Dhaka"
+print("I live in "+MyHome)
+
+#5.assign a single value to multiple variables
+Home=MyAddress=homeTown="Mohadevpur, Naogaon"
+print("My home town is",Home)
+
+#6.input float value using input()
+floatVal = float(input("Enter a Float value: "))
+print("The value is ",floatVal)
+
+#7.convert into uppercase
+a = "Welcome to Django web and Ai"
+print(a.upper())
+
+#8.remove whitespace
+b = "       python"
+print(b.strip())
+
+#9.convert int to float
+c = 60
+d = 22
+print('Int to float= ',float(c))
+print('Int to float= ',float(d))
+
+#10.create a dictonary & print your info
+myDictionary = {
+    "name": "Rangon Kumar Saha",
+    "Age": 21,
+    "cgpa": 3.5,
+    "dept": "Computer Science & Engineering"
+}
+print(myDictionary)
